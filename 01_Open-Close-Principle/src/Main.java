824adac59c0eef5cc99db110c4dcdc6e7ae692c9
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-04-28 10:55:49 +0530

    Content Updated

---
+++ 01_Open-Close-Principle/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author : ShEnUx
+ * @time : 10:55 PM
+ * @date : 4/28/2023
+ * @since : 0.1.0
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
